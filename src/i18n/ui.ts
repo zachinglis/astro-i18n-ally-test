22899d4b72d3f47499bf825798485e5a03fee1cb
Zach Inglis <zach@superhero-studios.com>
2023-12-10 06:05:25 +0000

    Initial commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    "home.welcome": "Welcome to Astro",
+    "home.cards.documentation": "Documentation",
+    "home.cards.documentation.description": "Learn how Astro works and explore the official API docs."
+  },
+  fr: {
+    "home.welcome": "Bienvenue à Astro",
+    "home.cards.documentation": "Documentation",
+    "home.cards.documentation.description": "Découvrez comment fonctionne Astro et explorez la documentation officielle de l'API."
+  }
+}
